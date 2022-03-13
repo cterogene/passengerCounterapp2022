28e1ce1fc2ead69243e884067a1a9364d566d17b
Terogene Claudio <cterogene@gmail.com>
2022-03-12 19:30:12 -0800

    update the app to add a save button and a previoious record.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let counter = 0
+let previousEl = document.getElementById("previous-el")
 function increment(){
     counter+= 1
     document.getElementById("count").innerText=counter
 }
+function save(){
+    let savedStr = " " + counter + " - "
+    previousEl.innerText += savedStr 
+
+    document.getElementById("count").innerText= counter = 0
+    }
